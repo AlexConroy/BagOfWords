cb0d16f4b16bef718b9b86e884d79a970b13f33f
Alex <alexdconroy@gmail.com>
2016-03-12 14:57:41 +0000

    Edited LoginActivity and xml file. Added new login screen with email and password validation methods

--- app/src/main/java/com/alex/bagofwords/LoginActivity.java
+++ app/src/main/java/com/alex/bagofwords/LoginActivity.java
@@ -1,22 +1,23 @@
 package com.alex.bagofwords;
 
-        import android.content.Context;
-        import android.content.Intent;
-        import android.support.design.widget.TextInputLayout;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.view.inputmethod.InputMethodManager;
-        import android.widget.Button;
-        import android.widget.EditText;
-        import android.widget.TextView;
-        import android.widget.Toast;
+import android.content.Context;
+import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-        import java.util.regex.Matcher;
-        import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 
 public class LoginActivity extends AppCompatActivity {
-
 
 
     @Override
@@ -24,9 +25,42 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        final EditText email = (EditText) findViewById(R.id.input_email);
+        final EditText password = (EditText) findViewById(R.id.input_password);
+        Button loginButton = (Button) findViewById(R.id.button_login);
+
+        assert loginButton != null;
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!validEmail(email.getText().toString())) {
+                    email.setError("Invalid Email");
+                    email.requestFocus();
+                } else if (!validPassword(password.getText().toString())) {
+                    password.setError("Invalid Password");
+                    password.requestFocus();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Validation Success", Toast.LENGTH_LONG);
+                }
+
+            }
+        });
+
 
     }
+    // Validation of email method.
+    protected boolean validEmail(String email) {
+        String emailPattern = "^[a-zA-Z0-9#_~!$&'()*+,;=:.\"(),:;<>@\\[\\]\\\\]+@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*$"; // Email regular expression.
+        Pattern pattern = Pattern.compile(emailPattern);
+        Matcher matcher = pattern.matcher(email);
 
+        return matcher.matches();
+    }
+
+    // Validation of password method (i.e. must be getter than 5)
+    protected boolean validPassword(String password) {
+        return password.length() >= 6;
+    }
 
 
 }
