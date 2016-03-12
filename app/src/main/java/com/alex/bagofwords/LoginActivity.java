a604cffe3403bad284329effd2b00ea932d40bf4
Alex <alexdconroy@gmail.com>
2016-03-12 13:35:48 +0000

    Edited LoginActivity and cml file. Removed material design in xml to converted to basic xml. Removed java functions and returned to empty onCreate method

--- app/src/main/java/com/alex/bagofwords/LoginActivity.java
+++ app/src/main/java/com/alex/bagofwords/LoginActivity.java
@@ -17,30 +17,6 @@ package com.alex.bagofwords;
 
 public class LoginActivity extends AppCompatActivity {
 
-    // Hide keyboard method.
-    private void hideKeyboard() {
-        View view = getCurrentFocus();
-        if(view != null) {
-            ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE)).
-                    hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-        }
-    }
-
-    // Valid username method.
-    protected boolean validateUsername(String username) {
-        String usernamePattern = "^[a-zA-Z0-9#_~!$&'()*+,;=:.\"(),:;<>@\\[\\]\\\\]+@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*$";
-
-        Pattern pattern = Pattern.compile(usernamePattern);
-        Matcher matcher = pattern.matcher(username);
-
-        return matcher.matches();
-    }
-
-    // Valid Password method.
-    protected boolean validatePassword(String password) {
-        return password.length() > 6;
-    }
-
 
 
     @Override
@@ -48,47 +24,6 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        final TextInputLayout usernameWrap = (TextInputLayout) findViewById(R.id.usernameWrapper);
-        final TextInputLayout passwordWrap = (TextInputLayout) findViewById(R.id.passwordWrapper);
-        Button loginBtnPressed = (Button) findViewById(R.id.button_login);
-        TextView signupPressed = (TextView) findViewById(R.id.link_signup);
-
-
-        // Click listener for login.
-        assert loginBtnPressed != null;
-        loginBtnPressed.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                hideKeyboard();
-
-                assert usernameWrap != null;
-                String username = usernameWrap.getEditText().getText().toString();
-                assert passwordWrap != null;
-                String password = passwordWrap.getEditText().getText().toString();
-                if (!validateUsername(username)) {
-                    usernameWrap.setError("Invalid Username");
-                } else if (!validatePassword(password)) {
-                    passwordWrap.setError("Invalid Password");
-                } else {
-                    usernameWrap.setErrorEnabled(false);
-                    passwordWrap.setErrorEnabled(false);
-                    Toast.makeText(getApplicationContext(), "Logging into account", Toast.LENGTH_LONG).show();
-                }
-            }
-        });
-
-        // Click listener for create account.
-        assert signupPressed != null;
-        signupPressed.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                hideKeyboard();
-                //Toast.makeText(getApplicationContext(), "Create Account", Toast.LENGTH_LONG).show();
-                Intent signupIntent = new Intent(getApplicationContext(), SignupActivity.class);
-                startActivity(signupIntent);
-
-            }
-        });
 
     }
 
