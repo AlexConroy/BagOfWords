4d7a525e8a4b49c48a824044447401cecebb9b69
Alex <alexdconroy@gmail.com>
2016-04-27 10:32:04 +0100

    Added more content to each GamePlay activity.

--- app/src/main/java/com/alex/bagofwords/AdvancedGamePlay.java
+++ app/src/main/java/com/alex/bagofwords/AdvancedGamePlay.java
@@ -1,13 +1,125 @@
 package com.alex.bagofwords;
 
+import android.content.ClipData;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.DragEvent;
+import android.view.View;
+import android.widget.Button;
 
 public class AdvancedGamePlay extends AppCompatActivity {
+
+    Button fieldOne;
+    Button fieldTwo;
+    Button fieldThree;
+    Button fieldFour;
+    Button fieldFive;
+    Button fieldSix;
+    Button fieldSeven;
+
+    Button returnBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advanced_game_play);
+
+        fieldOne = (Button) findViewById(R.id.firstBtn);
+        fieldTwo = (Button) findViewById(R.id.secondBtn);
+        fieldThree = (Button) findViewById(R.id.thirdBtn);
+        fieldFour = (Button) findViewById(R.id.fourthBtn);
+        fieldFive = (Button) findViewById(R.id.fifthBtn);
+        fieldSix = (Button) findViewById(R.id.sixthBtn);
+        fieldSeven = (Button) findViewById(R.id.sixthBtn);
+
+        findViewById(R.id.firstBtn).setOnLongClickListener(longListen);
+        findViewById(R.id.secondBtn).setOnLongClickListener(longListen);
+        findViewById(R.id.thirdBtn).setOnLongClickListener(longListen);
+        findViewById(R.id.fourthBtn).setOnLongClickListener(longListen);
+        findViewById(R.id.fifthBtn).setOnLongClickListener(longListen);
+        findViewById(R.id.sixthBtn).setOnLongClickListener(longListen);
+        findViewById(R.id.seventhBtn).setOnLongClickListener(longListen);
+
+        findViewById(R.id.firstBtn).setOnDragListener(DropListner);
+        findViewById(R.id.secondBtn).setOnDragListener(DropListner);
+        findViewById(R.id.thirdBtn).setOnDragListener(DropListner);
+        findViewById(R.id.fourthBtn).setOnDragListener(DropListner);
+        findViewById(R.id.fifthBtn).setOnDragListener(DropListner);
+        findViewById(R.id.sixthBtn).setOnDragListener(DropListner);
+        findViewById(R.id.seventhBtn).setOnDragListener(DropListner);
+
     }
+
+    View.OnLongClickListener longListen = new View.OnLongClickListener() {
+        public boolean onLongClick(View v) {
+
+            DragShadow dragShadow = new DragShadow(v);
+
+
+            ClipData data = ClipData.newPlainText("","");
+            v.startDrag(data, dragShadow, v, 0);
+            return false;
+        }
+
+    };
+
+    private class DragShadow extends View.DragShadowBuilder {
+
+        ColorDrawable greyBox;
+
+        public DragShadow(View v) {
+            super(v);
+            greyBox = new ColorDrawable(Color.LTGRAY);
+        }
+
+        @Override
+        public void onDrawShadow(Canvas canvas) {
+            greyBox.draw(canvas);
+        }
+
+        @Override
+        public void onProvideShadowMetrics(Point shadowSize, Point shadowTouchPoint) {
+            View v = getView();
+            int height = (int) v.getHeight();
+            int width = (int) v.getWidth();
+
+            greyBox.setBounds(0, 0, width, height);
+            shadowSize.set(width, height);
+            shadowTouchPoint.set((int)width/2, (int)height/2);
+        }
+    }
+
+    View.OnDragListener DropListner = new View.OnDragListener() {
+
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            int dragEvent = event.getAction();
+
+            switch (dragEvent) {
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    Log.i("Drag Event", "Entered");
+                    break;
+
+                case DragEvent.ACTION_DRAG_EXITED:
+                    Log.i("Drag Event", "Exited");
+                    break;
+
+                case DragEvent.ACTION_DROP:
+
+                    Button target = (Button) v;
+                    Button dragged = (Button) event.getLocalState();
+                    String targetInitialText = (String) target.getText();
+                    target.setText(dragged.getText());
+                    dragged.setText(targetInitialText);
+                    break;
+            }
+            return true;
+        }
+    };
+
 }
