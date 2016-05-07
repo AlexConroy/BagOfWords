8052b51432913e4398a12a386f0f24e532e8df09
Alex <alexdconroy@gmail.com>
2016-05-07 23:37:12 +0100

    Testing

---
+++ app/src/test/java/com/alex/bagofwords/TestClassTest.java
@@ -0,0 +1,13 @@
+package com.alex.bagofwords;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.junit.Assert.*;
+
+public class TestClassTest {
+
+
+}
